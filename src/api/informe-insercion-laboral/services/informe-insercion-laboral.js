0a2b0341036fa3fc4706d2bcc02e543648ad6e0a
John Silva <jhonmvl18@gmail.com>
2022-07-13 16:13:10 -0500

    addedd external link on docs

---
+++ src/api/informe-insercion-laboral/services/informe-insercion-laboral.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * informe-insercion-laboral service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::informe-insercion-laboral.informe-insercion-laboral');
---
+++ src/api/informe-programa-capacitacion/services/informe-programa-capacitacion.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * informe-programa-capacitacion service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::informe-programa-capacitacion.informe-programa-capacitacion');
