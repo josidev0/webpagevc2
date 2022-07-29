34b95183688e51211a656578ab973181345f0be5
John Silva <jhonmvl18@gmail.com>
2022-07-29 13:03:48 -0500

    feat: new version && add spanish translation

---
+++ src/admin/app.js
@@ -0,0 +1,25 @@
+import spanishTranslations from "./extensions/translations/es.json";
+import favicon from "./extensions/icons/favicon.png";
+export default {
+  config: {
+    auth: {
+      logo: favicon,
+    },
+    head: {
+      favicon: favicon,
+    },
+    // Add a new locale, other than 'en'
+    locales: ["es"],
+
+    // Extend the translations
+    translations: {
+      es: spanishTranslations,
+    },
+    // Disable video tutorials
+    tutorials: false,
+    // Disable notifications about new Strapi releases
+    notifications: { release: false },
+  },
+
+  bootstrap() {},
+};
