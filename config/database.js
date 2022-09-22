b434282fac7ab55ce470ac5ae494f4aaf2e1669d
John Silva <jhonmvl18@gmail.com>
2022-09-21 20:46:42 -0500

    feat: localbase

--- config/database.js
+++ config/database.js
@@ -1,3 +1,18 @@
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: "mysql",
+//     connection: {
+//       host: env("DATABASE_HOST", "vinculacion2.espoch.edu.ec"),
+//       port: env.int("DATABASE_PORT", 3306),
+//       database: env("DATABASE_NAME", "c1sitevinc"),
+//       user: env("DATABASE_USERNAME", "c1uservinc"),
+//       password: env("DATABASE_PASSWORD", "Esp0chV1nc#"),
+//       ssl: false,
+//     },
+//     debug: false,
+//   },
+// });
+
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
