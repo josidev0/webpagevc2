b74b20bbcfc949f5a7e91eb0f45a578482e1c6a5
John Silva <jhonmvl18@gmail.com>
2023-04-20 16:07:28 -0500

    feat(continuing-education): :sparkles: added staff api

---
+++ src/api/educacion-continua-personal/controllers/educacion-continua-personal.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  educacion-continua-personal controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::educacion-continua-personal.educacion-continua-personal');
---
+++ src/api/educacion-continua-personal/routes/educacion-continua-personal.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * educacion-continua-personal router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::educacion-continua-personal.educacion-continua-personal');
