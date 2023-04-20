03eae2bf389f6075992a04ad9c2db856a582e8ac
John Silva <jhonmvl18@gmail.com>
2023-04-20 17:32:00 -0500

    feat(config): :sparkles: added config for document titles

---
+++ src/api/docs-config/controllers/docs-config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  docs-config controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::docs-config.docs-config');
---
+++ src/api/docs-config/routes/docs-config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * docs-config router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::docs-config.docs-config');
