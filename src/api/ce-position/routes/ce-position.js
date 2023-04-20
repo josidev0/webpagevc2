2fa759dd33506a280d1797a21029891a6b1f9bec
John Silva <jhonmvl18@gmail.com>
2023-04-20 16:33:09 -0500

    feat(continuing-education): :sparkles: added job position api

---
+++ src/api/ce-position/routes/ce-position.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ce-position router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::ce-position.ce-position');
---
+++ src/api/ce-position/services/ce-position.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ce-position service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::ce-position.ce-position');
