31b11437b74468232898173d15d6bccf00f1a0ab
John Silva <jhonmvl18@gmail.com>
2023-04-20 15:51:28 -0500

    feat(carousel): :sparkles: added carousel  for continuing education

---
+++ src/api/carrusel-educacion-continua/controllers/carrusel-educacion-continua.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  carrusel-educacion-continua controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::carrusel-educacion-continua.carrusel-educacion-continua');
---
+++ src/api/carrusel-educacion-continua/services/carrusel-educacion-continua.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * carrusel-educacion-continua service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::carrusel-educacion-continua.carrusel-educacion-continua');
