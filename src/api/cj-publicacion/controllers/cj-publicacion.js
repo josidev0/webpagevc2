cb274d06a48e49be9ed0c588b9321323916bff75
John Silva <jhonmvl18@gmail.com>
2023-04-20 17:03:56 -0500

    feat(trade): :sparkles: added publications api

---
+++ src/api/cj-publicacion/controllers/cj-publicacion.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  cj-publicacion controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::cj-publicacion.cj-publicacion');
---
+++ src/api/cj-publicacion/routes/cj-publicacion.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * cj-publicacion router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::cj-publicacion.cj-publicacion');
